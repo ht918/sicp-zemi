0c802a438261ffe71f9a09414e8d7fee2f6f0283
Hiroki Tsuboi <pineappler@Hirokis-MacBook-Air.local>
2017-01-18 17:21:13 +0900

    add apply-generic zero?

--- chap2/apply-generic.scm
+++ chap2/apply-generic.scm
@@ -63,22 +63,22 @@
       (error "Bad tagged datum: CONTENTS" datum )))
 
 ;;ex2.78
-(define (attach-tag type-tag contents)
-  (if (eq? type-tag 'scheme-number)
-      contents
-      (cons type-tag contents )))
-(define (type-tag datum)
-  (if (pair? datum)
-      (car datum)
-      (if (number? datum)
-          'scheme-number
-          (error "Bad tagged datum: TYPE-TAG" datum ))))
-(define (contents datum)
-  (if (pair? datum)
-      (cdr datum)
-      (if (number? datum)
-          datum
-          (error "Bad tagged datum: CONTENTS" datum ))))
+;(define (attach-tag type-tag contents)
+;  (if (eq? type-tag 'scheme-number)
+;      contents
+;      (cons type-tag contents )))
+;(define (type-tag datum)
+;  (if (pair? datum)
+;      (car datum)
+;      (if (number? datum)
+;          'scheme-number
+;          (error "Bad tagged datum: TYPE-TAG" datum ))))
+;(define (contents datum)
+;  (if (pair? datum)
+;      (cdr datum)
+;      (if (number? datum)
+;          datum
+;          (error "Bad tagged datum: CONTENTS" datum ))))
 
 (define (apply-generic op . args)
   (let (( type-tags (map type-tag args )))
@@ -126,4 +126,4 @@
 (define (mul x y) (apply-generic 'mul x y))
 (define (div x y) (apply-generic 'div x y))
 
-
+(define (=zero? n) (apply-generic '=zero? n))
