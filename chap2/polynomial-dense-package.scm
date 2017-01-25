bee41c9946f3bed2b06c00d078be66d8746425c8
Hiroki Tsuboi <pineappler@Hirokis-MacBook-Air.local>
2017-01-25 16:35:58 +0900

    ex2.91

--- chap2/polynomial-dense-package.scm
+++ chap2/polynomial-dense-package.scm
@@ -61,6 +61,30 @@
             (make-term (+ (order t1) (order t2))
                        (mul (coeff t1) (coeff t2)))
             (mul-term-by-all-terms t1 (rest-terms L))))))
+  
+ (define (div-terms L1 L2)
+    (if (empty-termlist? L1)
+        (list (the-empty-termlist) (the-empty-termlist ))
+        (let ((t1 (first-term L1))
+              (t2 (first-term L2)))
+          (if (> (order t2) (order t1))
+              (list (the-empty-termlist) L1)
+              (let ((new-c (div (coeff t1) (coeff t2)))
+                    (new-o (- (order t1) (order t2))))
+                (let ((rest-of-result
+                       (let ((rest (add-terms L1
+                                              (inversion
+                                               (mul-terms L2 (list (make-term new-o new-c)))))))
+                         (div-terms rest L2))))
+                  (cons (adjoin-term (make-term new-o new-c)
+                                     (car rest-of-result))
+                        (cdr rest-of-result))
+                  ))))))
+  (define (div-poly p1 p2)
+     (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (div-terms (term-list p1) (term-list p2)))
+        (error "Polys not in same var: ADD-POLY" (list p1 p2))))
     
   (define (adjoin-term term term-list)
     (if (=zero? (coeff term ))
