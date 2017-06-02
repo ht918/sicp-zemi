cb537372d2636f51bedfec549716e550a8d1ad23
Hiroki Tsuboi <pineappler@Hirokis-MacBook-Air.local>
2017-06-02 11:59:16 +0900

    change junban

--- chap4/environments.scm
+++ chap4/environments.scm
@@ -94,16 +94,6 @@
 
 
 ;chap4.1.4
-(define (setup-environment)
-  (let (( initial-env
-         (extend-environment (primitive-procedure-names)
-                             (primitive-procedure-objects)
-                             the-empty-environment )))
-    (define-variable! 'true true initial-env)
-    (define-variable! 'false false initial-env)
-    initial-env ))
-(define the-global-environment (setup-environment ))
-
 (define (primitive-procedure? proc)
   (tagged-list? proc 'primitive ))
 (define (primitive-implementation proc) (cadr proc))
@@ -126,6 +116,18 @@
 (define (primitive-procedure-objects)
   (map (lambda (proc) (list 'primitive (cadr proc )))
        primitive-procedures ))
+
+(define (setup-environment)
+  (let (( initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment )))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env ))
+(define the-global-environment (setup-environment ))
+
+
 (define (apply-primitive-procedure proc args)
   (apply-in-underlying-scheme
     (primitive-implementation proc) args))
